9d69c958ae911e09c969af47036205e2481f7dc9
John Traas <j.j.h.traas@gmail.com>
2020-08-07 12:41:49 +0000

    added sorting + product counts to product page + searching

--- products/views.py
+++ products/views.py
@@ -23,6 +23,9 @@ def all_products(request):
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
 
+            if sortkey == 'category':
+                sortkey = 'category__name'
+
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -40,7 +43,7 @@ def all_products(request):
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(category__name=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
