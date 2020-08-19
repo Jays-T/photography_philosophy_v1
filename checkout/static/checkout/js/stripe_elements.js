f41dcfc088686d7ec68adaa68e23f9ec9bc0584a
John Traas <j.j.h.traas@gmail.com>
2020-08-19 19:39:11 +0000

    added stripe elements + more checkout css styling

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,32 @@
+/* Core of the code here was taken from
+https://stripe.com/docs/payments/accept-a-payment
+
+CSS from:
+https://stripe.com/docs/stripe-js
+*/
+
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+var style = {
+      base: {
+        color: "#000",
+        fontFamily: 'Arial, sans-serif',
+        fontSmoothing: "antialiased",
+        fontSize: "16px",
+        "::placeholder": {
+          color: "#32325d"
+        }
+      },
+      invalid: {
+        fontFamily: 'Arial, sans-serif',
+        color: "#fa755a",
+        /*dc3545*/
+        iconColor: "#fa755a"
+      }
+    };
+var card = elements.create('card', {style: style});
+
+
+card.mount('#card-element');
