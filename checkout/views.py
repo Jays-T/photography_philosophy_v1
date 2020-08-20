b2099988c06ff957ce35a70c6127bf8f0016a86a
John Traas <j.j.h.traas@gmail.com>
2020-08-20 14:11:26 +0000

    setting up checkout view to handle POST method

--- checkout/views.py
+++ checkout/views.py
@@ -12,19 +12,35 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.error(request, "Oops, your bag is empty!")
-        return redirect(reverse('products'))
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
 
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        form_data = {
+            '': request.POST[''],
+            '': request.POST[''],
+            '': request.POST[''],
+            '': request.POST[''],
+            '': request.POST[''],
+            '': request.POST[''],
+            '': request.POST[''],
+            '': request.POST[''],
+            '': request.POST[''],
+        }
+        order_form = OrderForm (form_data)
+    else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, "Oops, your bag is empty!")
+            return redirect(reverse('products'))
+
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
 
     order_form = OrderForm()
     if not stripe_public_key:
