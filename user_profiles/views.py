c8404054747a3a13e77afc8ead27f418411f3929
John Traas <j.j.h.traas@gmail.com>
2020-08-26 22:42:30 +0000

    added view for update shipping address form in profile.html

--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+
 from .models import UserProfile
 from .forms import UserProfileForm
 
@@ -6,13 +8,21 @@ from .forms import UserProfileForm
 def profile(request):
 
     profile = get_object_or_404(UserProfile, user=request.user)
-    # orders = profile.orders.all()
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Shipping Address updated successfully')
+
     form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
 
     template = 'user_profiles/profile.html'
     context = {
         'form': form,
-        # 'orders': orders,
+        'orders': orders,
+        'from_profile_page': True,
     }
 
     return render(request, template, context)
