2080022776bdb04da729bc070c0c9a7ae255fd5b
John Traas <j.j.h.traas@gmail.com>
2020-08-31 03:17:20 +0000

    added about.html and contact.html templates + favicon

--- home/views.py
+++ home/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 
 
-# Create your views here.
-
-
 def index(request):
     """ A view to return index page """
 
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """ A view to return about page """
+
+    return render(request, 'home/about.html')
+
+
+def contact(request):
+    """ A view to return contact page """
+
+    return render(request, 'home/contact.html')
--- products/static/products/js/products.js
+++
@@ -1,4 +0,0 @@
-$('#new-image').change(function() {
-    var file = $('#new-image')[0].files[0];
-    $('#upload-text').text(`Image will be set to: ${file.name}`);
-});
