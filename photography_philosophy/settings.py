f7a8318537560a25552584c0685482c2c9403cae
John Traas <59046774+Jays-T@users.noreply.github.com>
2021-07-06 19:50:43 +0200

    Update settings.py

--- photography_philosophy/settings.py
+++ photography_philosophy/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'FALSE' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['photography-philosophy.herokuapp.com', 'localhost']
 
